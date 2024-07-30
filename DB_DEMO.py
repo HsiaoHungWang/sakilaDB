abfd725d8d6712ecad9a05a17206ab56e05d35fd
HsiaoHungWang <wang@ispan.com.tw>
2024-07-30 21:06:42 +0800

    讀取單筆資料

--- DB_DEMO.py
+++ DB_DEMO.py
@@ -4,7 +4,8 @@ class Category():
     def __init__():
         pass
 
-    def categories():
+        
+    def all():
        # 其它程式
 
         # Step1 建立連線
@@ -27,6 +28,34 @@ class Category():
                     cursor.execute(sql)
                     # step 3-1
                     results = cursor.fetchall()
+                    return results
+                except MySQLdb.MySQLError as e:
+                    print(f"資料讀取錯誤：{e}")
+                    return None
+
+    def single(id):
+       # 其它程式
+
+        # Step1 建立連線
+        connection = MySQLdb.connect(
+            host='localhost',
+            database='sakila',
+            user='root',
+            password='P@ssw0rd'
+        )
+        if not connection:
+            return None
+        
+        # Step2 SQL語法
+        sql = 'SELECT * FROM category WHERE category_id=%s'
+        
+        # Step3 建立Cursor物件執行SQL語法
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(sql,(id,))
+                    # step 3-1
+                    results = cursor.fetchone()
                     return results
                 except MySQLdb.MySQLError as e:
                     print(f"資料讀取錯誤：{e}")
